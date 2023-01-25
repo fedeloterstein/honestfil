87e7f724f66d8855feb9cfda415e9ba4364a67eb
fedeloterstein <federicoloterstein@gmail.com>
2023-01-25 00:02:08 -0300

    feat: create porSuccessModal

--- components/ui/SubmitNewPoRSection.tsx
+++ components/ui/SubmitNewPoRSection.tsx
@@ -1,9 +1,28 @@
-import { Button, Input, Select, Stack, Text } from '@chakra-ui/react'
+import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalOverlay, Select, Stack, Text, useDisclosure } from '@chakra-ui/react'
+import Image from 'next/image'
 import React from 'react'
 import { PlusWhiteIcon } from '../icons'
 
 export const SubmitNewPoRSection = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
+    <>
+       <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                <ModalOverlay bg={'rgba(225, 225, 225, 0.9)'}/>
+                <ModalContent maxH={'321px'} maxWidth={'509px'} borderRadius={'20px'} boxShadow={'0px 4px 20px 0px rgba(189, 236, 241, 0.5)'}>
+                    <ModalBody width={'100%'}>
+                        <Stack align={'center'}>
+                            <Image src={'/por.png'} alt={'algo'} width="155" height="155" />
+                            <Text fontWeight={700} fontSize={'20px'} lineHeight={'25px'} bgClip='text' bgGradient='linear(to-r, rgba(18, 127, 201, 1), rgba(18, 201, 157, 1))'>New Proof of reserve Add</Text>
+                            <Text align={'center'} fontWeight={400} fontSize={'12px'} lineHeight={'15px'}>Thank you for helping us build a more honest and 
+transparent ecosystem in Filecoin.</Text>
+                        </Stack>
+                    </ModalBody>
+                    <ModalFooter justifyContent={'center'}>
+                        <Button onClick={onClose} bgGradient={'linear(to-r, #127FC9, #12C99D)'} borderRadius={'50px'} p={'7px 20px'} color={'white'}>Go to Dashboard</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
     <Stack width={'387px'} minHeight={'382px'} align={'center'} borderRadius={'20px'} padding={'18px 20px'} justify={'space-around'} boxShadow={'0px 4px 20px 0px rgba(189, 236, 241, 0.5)'} gap={'14px'}>
         <Text fontWeight={600} fontSize={'16px'} lineHeight={'24px'}   bgClip='text' bgGradient='linear(to-r, rgba(18, 127, 201, 1), rgba(18, 201, 157, 1))'>Submit New Proof of Reserve</Text>
         <Input height={'48px'} borderRadius={'50px'} placeholder='Name Protocol or Dapp*'/>
@@ -11,7 +30,8 @@ export const SubmitNewPoRSection = () => {
         <Input height={'48px'} borderRadius={'50px'}  placeholder='Url Logo*'/>
         <Input height={'48px'} borderRadius={'50px'}  placeholder='Address Vault*'/>
         <Input  height={'48px'} borderRadius={'50px'} placeholder='Website*'/>
-        <Button leftIcon={<PlusWhiteIcon />} bgGradient={'linear(to-r, #127FC9, #12C99D)'} width={'100%'} borderRadius={'50px'} p={'0px 20px'} color={'white'}>Submit Proof Reserve</Button>
+        <Button onClick={onOpen} leftIcon={<PlusWhiteIcon />} bgGradient={'linear(to-r, #127FC9, #12C99D)'} width={'100%'} borderRadius={'50px'} p={'0px 20px'} color={'white'}>Submit Proof Reserve</Button>
     </Stack>
+    </>
   )
 }
